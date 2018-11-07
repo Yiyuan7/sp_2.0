1aa5dfaac1a7e758949eac39386b1d6fa6110ba1
Yiyuan7 <31291519+Yiyuan7@users.noreply.github.com>
2018-11-06 21:59:21 -0500

    add all

---
+++ Assets/chooselogo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chooselogo : MonoBehaviour {
+    public GameObject desk;
+    public GameObject chair;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Desk(){
+
+        desk.GetComponent<UnityEngine.XR.iOS.UnityARHitTestExample>().enabled = true;
+        chair.GetComponent<UnityEngine.XR.iOS.UnityARHitTestExample>().enabled = false;
+
+    }
+
+    public void Chair()
+    {
+        chair.GetComponent<UnityEngine.XR.iOS.UnityARHitTestExample>().enabled = true;
+        desk.GetComponent<UnityEngine.XR.iOS.UnityARHitTestExample>().enabled = false;
+    }
+     
+
+}
